abf96a788ba5afe42a5a78ddc48879a0363e3af2
Rafeequl <rafeequl@gmail.com>
2012-04-16 10:27:13 +0700

    Initial import.

---
+++ README.md
@@ -0,0 +1,23 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* jquery
+* mysql
+* rails_basics
+* git
+* rspec
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messageapp::Application.config.secret_token = 'ef15cddb4e0180515051149ef25435987dd1870bc2c034515026a72e8547c34fc08c485518b63b08fff03302476e2db700715eb67d4bc3860433c4f527894f52'
